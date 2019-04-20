155eca77c7c4281c9f6e67f1c102f6cee98a17d1
Vetrivel Chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2019-04-21 01:34:17 +0530

    added color palettes

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 const canvasSketch = require('canvas-sketch');
 const {lerp} = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [ 2048, 2048 ],
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+   const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
+  //  console.log(palette);
+
   const createGrid = () => {
     const points = [];
     const count = 17;
@@ -15,6 +20,7 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1)
         const v =  count <= 1 ? 0.5 : y / (count - 1)
         points.push({
+          color:random.pick(palette),
           radius: Math.abs(0.01 + random.gaussian() * 0.01),
           position: [u, v]
         });
@@ -33,7 +39,8 @@ const sketch = () => {
     points.forEach(data => {
      const {
        position,
-       radius
+       radius,
+       color
      } = data;
      
      const [u, v] = position;
@@ -43,7 +50,7 @@ const sketch = () => {
      
      context.beginPath();
      context.arc(x,y, radius * width, 0, Math.PI * 2, false);
-      context.fillStyle = 'red';
+      context.fillStyle = color;
      context.fill();
     });
   };
