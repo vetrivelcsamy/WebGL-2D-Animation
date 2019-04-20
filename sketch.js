e6df1496b5e7be1947ad9d5d05636e806c2ab232
Vetrivel Chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2019-04-21 01:55:59 +0530

    added random color chart

--- sketch.js
+++ sketch.js
@@ -8,27 +8,27 @@ const settings = {
 };
 
 const sketch = () => {
-  const colorCount = random.rangeFloor(1, 6);
+  const colorCount = random.rangeFloor(2, 6);
    const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
   //  console.log(palette);
 
   const createGrid = () => {
     const points = [];
-    const count = 17;
+    const count = 30;
     for (let x = 0; x < count; x++){
       for(let y = 0; y < count; y++){
-        const u = count <= 1 ? 0.5 : x / (count - 1)
-        const v =  count <= 1 ? 0.5 : y / (count - 1)
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v =  count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v) * 0.05);
         points.push({
           color:random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          radius,
           position: [u, v]
         });
       }
     }
     return points;
   };
-  random.setSeed(512);
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
