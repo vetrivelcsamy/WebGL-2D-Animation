f20c3f0e9eccd91d1305fd3eb84540b7710ab51c
Vetrivel Chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2019-04-21 02:20:47 +0530

    part of final out one

--- sketch.js
+++ sketch.js
@@ -19,10 +19,11 @@ const sketch = () => {
       for(let y = 0; y < count; y++){
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v =  count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v) * 0.05);
+        const radius = Math.abs(random.noise2D(u, v) * 0.1);
         points.push({
           color:random.pick(palette),
           radius,
+          rotation: random.noise2D(u,v),
           position: [u, v]
         });
       }
@@ -40,7 +41,8 @@ const sketch = () => {
      const {
        position,
        radius,
-       color
+       color,
+       rotation
      } = data;
      
      const [u, v] = position;
@@ -48,10 +50,19 @@ const sketch = () => {
      const x = lerp(margin, width - margin, u);
      const y = lerp(margin, height - margin, v);
      
-     context.beginPath();
-     context.arc(x,y, radius * width, 0, Math.PI * 2, false);
-      context.fillStyle = color;
-     context.fill();
+    //  context.beginPath();
+    //  context.arc(x,y, radius * width, 0, Math.PI * 2, false);
+    //   context.fillStyle = color;
+    //  context.fill();
+
+    context.save()
+    context.fillStyle = color;
+    context.font = `${radius * width}px "Helvetcia"`;
+    context.translate(x, y);
+    context.rotate(rotation);
+    context.fillText('+', x, y);
+
+    context.restore();
     });
   };
 };
